d5d73aa5afea03c2bd1cf954316f47fcf7204d7b
MaksymKondratenko <maxym.kondratenko@gmail.com>
2020-09-09 17:17:24 +0200

    adds cost function for logistic regression

--- supervised_learning/logistic_regression/costFunction.m
+++ supervised_learning/logistic_regression/costFunction.m
@@ -6,7 +6,7 @@ k = -1/m
 costSum = 0;
 for i = 1:m,
     z = theta' * extractFeatureVector(X, i);
-    hXi = 1/ (1 + e^z);
+    hXi = 1/ (1 + e^-z);
     costI = y(i) * log(hXi) + (1 - y(i)) * log(1 - hXi);
     costSum = costSum + costI;
     i++;
