27aa98b9b2bacaf3a52ffa0dcf616930fd283b28
Maksym <maxym.kondratenko@gmail.com>
2020-09-09 08:45:00 +0200

    adds feature scaling

--- learn.m
+++ learn.m
@@ -1,9 +1,12 @@
 function theta = learn(X, y, theta, alpha, threshold);
 
+X_scaled = scaleFeatures(X);
+
 iteration = 1;
-while costFunction(X, y, theta) > threshold
+while costFunction(X_scaled, y, theta) > threshold
   disp(sprintf('iteration %1.0f', iteration));
-  theta = gradientDescent(X, y, theta, alpha)
+  theta = gradientDescent(X_scaled, y, theta, alpha)
   iteration++;
 end;
 
+theta = deScaleTheta(X, theta)
---
+++ scaleFeatures.m
@@ -0,0 +1,8 @@
+function X = scaleFeatures(X);
+
+maxXj = max(X)
+maxX = max(maxXj)
+
+if maxX > 1 || maxX < -1,
+  X = X ./ max(X)
+end;
