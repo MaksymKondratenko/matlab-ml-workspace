116cff6d56d85c2e46c9f59b49a4260bb2c96a5c
MaksymKondratenko <maxym.kondratenko@gmail.com>
2020-09-09 10:59:09 +0200

    adds vectorization to gradient descent function

--- gradientDescent.m
+++ gradientDescent.m
@@ -3,10 +3,10 @@ function T = gradientDescent(X, y, theta, alpha);
 m = size(X, 1);
 k = alpha/m;
 
-hX = X*theta
-delta = hX-y
-
-for i=1:size(theta),
-  T(i,1) = theta(i) - k* sum(delta .* X(:, i));
+delta = zeros(size(theta));
+for i=1:m,
+  trainingRecord = X(i,:)';
+  delta = delta + (theta'*trainingRecord - y(i)) * trainingRecord;
 end;
-T
+
+T = theta - k * delta;
