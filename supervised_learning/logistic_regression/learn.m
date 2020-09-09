9b73f5a7c5a568c63fc1f69f27e9a9de38d510d2
MaksymKondratenko <maxym.kondratenko@gmail.com>
2020-09-09 21:43:33 +0200

    adds accuracy and plot to the learning algo

--- supervised_learning/logistic_regression/learn.m
+++ supervised_learning/logistic_regression/learn.m
@@ -3,11 +3,18 @@ disp('Scaling features');
 X_scaled = scaleFeatures(X);
 disp('Learning started');
 
-for i = 1:3000
-  disp(sprintf('iteration %1.0f', i));
+accuracy = 0.01;
+iteration = 1;
+while accuracy < 0.95 && iteration <= 2000,
+  disp(sprintf('iteration %1.0f', iteration));
   cost = costFunction(X_scaled, y, theta)
   theta = gradientDescent(X_scaled, y, theta, alpha);
+  accuracy = mean(prediction(X, deScaleTheta(X, theta)) >= 0.5 == y)
+  iteration++;
+  diagram(iteration, 1) = accuracy;
+  diagram(iteration, 2) = cost;
 end;
 
+plot(diagram);
 disp('Training finished. Reverting scale.');
 theta = deScaleTheta(X, theta)
