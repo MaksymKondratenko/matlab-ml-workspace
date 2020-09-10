c949c04ec0d640c1eb54f4ab5d2bc5aab05bfccf
MaksymKondratenko <maxym.kondratenko@gmail.com>
2020-09-10 21:40:32 +0200

    fixes regularization parameter in logistic regression

--- supervised_learning/logistic_regression/costFunction.m
+++ supervised_learning/logistic_regression/costFunction.m
@@ -2,6 +2,6 @@ function J = costFunction(X, y, theta, lambda);
 
 m = size(X, 1);
 hX = prediction(X, theta);
-regParam = lambda / (2 * m) * sum(theta.^2);
+regParam = lambda / (2 * m) * sum(theta(2:end).^2);
 
 J = 1/m * (-y' * log(hX) - (1 - y)' * log(1 - hX)) + regParam;
--- supervised_learning/logistic_regression/gradientDescent.m
+++ supervised_learning/logistic_regression/gradientDescent.m
@@ -3,6 +3,6 @@ function T = gradientDescent(X, y, theta, alpha, lambda);
 m = size(X, 1);
 k = alpha/m;
 hX = prediction(X, theta);
-regParam = lambda/m * theta
+regParam = lambda/m * [0; theta(2:end)]
 
 T = theta - k * X' * (hX - y) + regParam;
