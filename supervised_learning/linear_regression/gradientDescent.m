323aa90e1e9a33a2def55e54793a48008ebdf874
MaksymKondratenko <maxym.kondratenko@gmail.com>
2020-09-10 15:01:05 +0200

    adds vectorized implementation for gradient descent of linear regression

--- supervised_learning/linear_regression/gradientDescent.m
+++ supervised_learning/linear_regression/gradientDescent.m
@@ -3,11 +3,4 @@ function T = gradientDescent(X, y, theta, alpha);
 m = size(X, 1);
 k = alpha/m;
 
-delta = zeros(size(theta));
-for i = 1:m,
-  featureVector = extractFeatureVector(X, i);
-  prediction = theta' * featureVector;
-  delta = delta + (prediction - y(i)) * featureVector;
-end;
-
-T = theta - k * delta;
+T = theta - k * X * (X * theta - y);
