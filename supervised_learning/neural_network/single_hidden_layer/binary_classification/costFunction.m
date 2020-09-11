ed48f25a528628f68a3b81adcd24e4d3917766c3
MaksymKondratenko <maxym.kondratenko@gmail.com>
2020-09-11 12:51:44 +0200

    adds cost function for binary classification neural network

---
+++ supervised_learning/neural_network/single_hidden_layer/binary_classification/costFunction.m
@@ -0,0 +1,15 @@
+function cost = costFunction(xi, y, thetaHidden, thetaOutput, lambda);
+
+    % xi is n x 1 since it is a feature vector
+    % thetaHidden is Sj x (Sj-1 + 1)
+    % thetaOutput is K x (Sj + 1)
+    m = size(X, 1);
+    J = zeros(m, 1)
+    for i = 1:m,
+        xi = X(i,:)'; % n x 1; j
+        yi = y(i,:)'; % K x 1
+        [a2, hxi] = prediction(xi, thetaHidden, thetaOutput); % [(Sj + 1) x 1, K x 1]
+        J(i) = -1/m * (-yi' * log(hxi) - (1 - yi)' * log(1 - hxi)) % 1 x 1
+    end
+    cost = sum(J);
+end
