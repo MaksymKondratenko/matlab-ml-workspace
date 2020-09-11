97ba2ffa8f1f5a2a396c73a73ca15b9e469bb124
MaksymKondratenko <maxym.kondratenko@gmail.com>
2020-09-11 12:09:07 +0200

    fixes forward propagation of binary classification neural network

--- supervised_learning/neural_network/single_hidden_layer/binary_classification/prediction.m
+++ supervised_learning/neural_network/single_hidden_layer/binary_classification/prediction.m
@@ -1,6 +1,8 @@
-function [a2, a3] = prediction(xi, thetaHidden, thetaOutput);
+function [a2wb, a3] = prediction(xi, thetaHidden, thetaOutput);
 
-    % thetaHidden is a Sj x (Sj-1 + 1), and xi is (Sj-1 + 1) x 1, since is a feature vector
+    % xi is (Sj-1 + 1) x 1, since is a feature vector
+    % thetaHidden is Sj x (Sj-1 + 1)
+    % thetaOutput is K x (Sj + 1)
     z2 = thetaHidden * xi; % Sj x 1
     a2 = sigmoid(z2); % Sj x 1
     a2wb = [1; a2]; % (Sj + 1) x 1
