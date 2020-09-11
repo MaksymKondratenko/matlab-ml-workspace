6bd752b62ecb71e882dc2b23a320cf7547e8fc94
MaksymKondratenko <maxym.kondratenko@gmail.com>
2020-09-11 12:58:08 +0200

    adds regularization for cost function of binary classification neural network

--- supervised_learning/neural_network/single_hidden_layer/binary_classification/costFunction.m
+++ supervised_learning/neural_network/single_hidden_layer/binary_classification/costFunction.m
@@ -6,10 +6,12 @@ function cost = costFunction(xi, y, thetaHidden, thetaOutput, lambda);
     m = size(X, 1);
     J = zeros(m, 1)
     for i = 1:m,
-        xi = X(i,:)'; % n x 1; j
+        xi = X(i,:)'; % n x 1
         yi = y(i,:)'; % K x 1
         [a2, hxi] = prediction(xi, thetaHidden, thetaOutput); % [(Sj + 1) x 1, K x 1]
-        J(i) = -1/m * (-yi' * log(hxi) - (1 - yi)' * log(1 - hxi)) % 1 x 1
+        J(i) = -1/m * (-yi' * log(hxi) - (1 - yi)' * log(1 - hxi)); % 1 x 1
     end
-    cost = sum(J);
+
+    regParam = lambda / (2 * m) * (sum(sum(thetaOutput.^2)) + sum(sum(thetaHidden.^2))); % 1 x 1
+    cost = sum(J) + regParam;
 end
